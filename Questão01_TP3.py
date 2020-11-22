1b7beb693f663a03c8fb6d0189c3da92b4c175fb
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2020-11-22 11:33:45 -0300

    Create Questão01_TP3.py

---
+++ Questão01_TP3.py
@@ -0,0 +1,41 @@
+def add_element(l):
+    l.append(1)
+    l.append(2)
+    l.append(3)
+    l.append(4)
+    l.append(5)
+    print("Essa é a lista com elementos adicionados:", l)
+    return l
+
+def verify_element_3(l):        
+    if 3 in l:
+        print("O elemento '3' está na lista e será removido!")
+        l.remove(3)
+    else:
+        print("O elemento '3' não está na lista")
+    return l
+
+def verify_element_6(l):
+    if 6 in l:
+        print("O elemento '6' está na lista...")
+        l.remove(6)
+    else:
+        print("O elemento '6' não está na lista...")
+    print("Essa é a lista com o elemento retirado:", l)
+    return l
+
+def print_size(l):
+    print(f"O número de elementos atual da lista é {len(l)}.")    
+    
+def change_element(l):
+    l[3] = 6
+    print(f"Essa é a lista com o último elemento trocado: {l}.")
+    
+
+l = []
+
+add_element(l)
+verify_element_3(l)
+verify_element_6(l)
+print_size(l)
+change_element(l)
