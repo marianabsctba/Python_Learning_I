5f9ddf25f47ef85d2cc176eefbf1eb5b01300bad
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2020-11-23 22:24:41 -0300

    Create Questões_17_a_22_TP3.py

---
+++ Questões_17_a_22_TP3.py
@@ -0,0 +1,17 @@
+#As questões 17 a 22 constam em um só arquivo e estão indicadas nos comentários.
+# Foi utilizado o arquivo original, mas com as modificações solicitadas.
+
+import pygame, sys, os
+from pygame.locals import *
+
+LARGURA_TELA = 750
+ALTURA_TELA = 500
+
+LARGURA_LINHA = 10
+PALETA_TAMANHO = 50
+PALETAOFFSET = 20
+
+PRETO = (0,0,0)
+BRANCO = (255,255,255)
+
+FPS = 200
